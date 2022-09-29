3e650fd9de06b2a190237c23ec136d1c55071d25
xinlimirages <540946044@qq.com>
2022-09-29 13:58:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'onlyp6BfVv7wzq2t0V67KUsk_M-4',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'd7QCBXwSU5znQIX6VhLxmh9PbujhqHja-D3kb4u30ow',
       // 所在省份
       province: '广东',
       // 所在城市
