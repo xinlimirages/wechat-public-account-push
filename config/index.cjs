ce73d4613853f57cccf20fa576c7c0578eb1d2c0
xinlimirages <540946044@qq.com>
2022-09-29 11:26:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,7 +12,7 @@ const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '33f9faf8e4f37b753e134d1fa0445f7c',
+  APP_SECRET: '0a997282fca1438adb09b838656fb703',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
